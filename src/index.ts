52b6f3563448a84fe343009011772dfbe71ab94c
Jinhuan Lei <jinhuanlei@gmail.com>
2020-04-11 13:13:12 -0500

    double click

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import {Observable} from 'rxjs';
+import {bufferWhen, filter, delay, debounceTime} from 'rxjs/operators'
 // @ts-ignore
 import * as helpers from './helpers'
 
-const observable1 = Observable.create((observer:any) => {
+const observable1 = Observable.create((observer: any) => {
     observer.next('Hello World!');
     observer.next('Hello Again!');
     observer.complete();
     observer.next('Bye');
 })
-const arr = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
-const observable2 = Observable.interval(400).take(arr.length).map(i => arr[i]);
+const data = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
+const observable2 = Observable.interval(400).take(data.length).map(i => data[i]);
 
 // add all the number together in this array, takes some time 400 * len
 const observable3 = observable2
@@ -18,10 +19,34 @@ const observable3 = observable2
     .reduce(((previousValue, currentValue) => previousValue + currentValue));
 
 // change the observable name to run it
-observable3.subscribe(
-    (x:any) => helpers.logItem(x),
-    (error: any) => helpers.logItem ('Error: ' + error),
-    () => helpers.logItem('Completed')
+// observable3.subscribe(
+//     (x:any) => helpers.logItem(x),
+//     (error: any) => helpers.logItem ('Error: ' + error),
+//     () => helpers.logItem('Completed')
+// );
+
+var button = document.querySelector('button');
+var label = document.getElementById('label');
+
+var clickStream = Observable.fromEvent(button, 'click');
+
+const dblClickStream = clickStream.pipe(
+    bufferWhen(() => clickStream.pipe(debounceTime(250))),
+    filter(arr => arr.length === 2)
 );
+
+
+dblClickStream.subscribe(event => {
+    label.textContent = 'double click';
+});
+
+dblClickStream.pipe(delay(1000))
+    .subscribe(suggestion => {
+        label.textContent = '-';
+    })
+
+
+
+
 
 
