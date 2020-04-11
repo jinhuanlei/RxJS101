d08643457731701be9d24500b1d17ebb6445c0f2
Jinhuan Lei <jinhuanlei@gmail.com>
2020-04-11 11:58:49 -0500

    observable3

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import {Observable} from 'rxjs';
 // @ts-ignore
 import * as helpers from './helpers'
 
-var observable1 = Observable.create((observer:any) => {
+const observable1 = Observable.create((observer:any) => {
     observer.next('Hello World!');
     observer.next('Hello Again!');
     observer.complete();
     observer.next('Bye');
 })
 const arr = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
-var observable2 = Observable.interval(400).take(arr.length).map(i => arr[i]);
+const observable2 = Observable.interval(400).take(arr.length).map(i => arr[i]);
 
+// add all the number together in this array, takes some time 400 * len
+const observable3 = observable2
+    .map(i => parseInt(i))
+    .filter(i => !isNaN(i))
+    .reduce(((previousValue, currentValue) => previousValue + currentValue));
 
-// change the observerable name to run it
-observable2.subscribe(
+// change the observable name to run it
+observable3.subscribe(
     (x:any) => helpers.logItem(x),
     (error: any) => helpers.logItem ('Error: ' + error),
     () => helpers.logItem('Completed')
